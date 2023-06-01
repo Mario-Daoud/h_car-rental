8bed274eb978c86d025a1409f22fbb5b6733bca1
Mario Daoud <r0895582@student.vives.be>
2023-06-01 21:43:10 +0200

    add: landing page and header hover styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderfooterModule } from './headerfooter/headerfooter.module';
+import { FeaturesModule } from './features/features.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HeaderfooterModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, FeaturesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
