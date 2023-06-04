e9ef2245830b8ec24bafaff8cd99ca1c823e2882
Mario Daoud <r0895582@student.vives.be>
2023-06-04 17:02:55 +0200

    add: plan-trip and promobanner components

--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingComponent } from './landing/landing.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { PlanTripComponent } from './plan-trip/plan-trip.component';
+import { PromobannerComponent } from './promobanner/promobanner.component';
 
 @NgModule({
-  declarations: [LandingComponent, HomepageComponent],
+  declarations: [LandingComponent, HomepageComponent, PlanTripComponent, PromobannerComponent],
   imports: [CommonModule],
   exports: [HomepageComponent],
 })
---
+++ src/app/homepage/promobanner/promobanner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promobanner',
+  templateUrl: './promobanner.component.html',
+  styleUrls: ['./promobanner.component.css']
+})
+export class PromobannerComponent {
+
+}
